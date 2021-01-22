129194ec5c1fb1e3184e50583a1e1480f282c9be
ranjithkumar <49120868+ranjithkumar121@users.noreply.github.com>
2021-01-22 11:49:18 +0530

    Add files via upload

---
+++ Alexa.py
@@ -0,0 +1,71 @@
+import speech_recognition as sr
+import pyttsx3
+import pywhatkit
+import datetime
+import wikipedia
+import pyjokes
+
+listener = sr.Recognizer()
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[1].id)
+
+
+def talk(text):
+    engine.say(text)
+    engine.runAndWait()
+
+
+def take_command():
+    try:
+        with sr.Microphone() as source:
+            print('listening...')
+            voice = listener.listen(source)
+            command = listener.recognize_google(voice)
+            command = command.lower()
+            if 'alexa' in command:
+                command = command.replace('alexa', '')
+                print(command)
+    except:
+        pass
+    return command
+
+
+def run_alexa():
+    command = take_command()
+    print(command)
+    if 'play' in command:
+        song = command.replace('play', '')
+        talk('playing ' + song)
+        pywhatkit.playonyt(song)
+    elif 'breakfast' in command:
+        talk('No.I not have stomach')
+    elif 'how are you' in command:
+        talk('I am fine')
+    elif 'who is your boss' in command:
+        talk('Mr Ranjith kumar who is a python developer.')
+    elif 'time' in command:
+        time = datetime.datetime.now().strftime('%I:%M %p')
+        talk('Current time is ' + time)
+    elif 'who is' in command:
+        person = command.replace('who is', '')
+        info = wikipedia.summary(person, 1)
+        print(info)
+        talk(info)
+    elif 'date' in command:
+        talk('sorry, I have a headache')
+    elif 'are you single' in command:
+        talk('I am in a relationship with wifi')
+    elif 'joke' in command:
+        talk(pyjokes.get_joke())
+    elif 'love you' in command:
+        talk('Love you too Ranjith')
+    elif 'bye' in command:
+        talk('Byee . see you')
+        breakpoint()
+    else:
+        talk('Please say the command again.')
+
+
+while True:
+    run_alexa()
